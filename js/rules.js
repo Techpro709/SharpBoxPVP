e7d08c6a9a451d7c317f1cc133dd7d10dbd31eab
TechPro709 <17007@mycva.org>
2023-05-22 23:46:33 -0700

    added more rules

--- js/rules.js
+++ js/rules.js
@@ -9,9 +9,39 @@ var rules = [
         rule:{
             text:'No naked killing'
         }
+    },
+    {
+        rule:{
+            text:'No Asking Admins for stuff or OP'
+        }
+    },
+    {
+        rule:{
+            text:'Don\'t be toxic or disrespectful to ANYONE including other players'
+        }
+    },
+    {
+        rule:{
+            text:'No swearing'
+        }
+    },
+    {
+        rule:{
+            text:'When the owner or an admin says something, it is final'
+        }
+    },
+    {
+        rule:{
+            text:'If you see a player hacking or an admin abuse of powers please report it on the discord <a style="color:blue;" href="https://discord.gg/8KdNwwFFbA">https://discord.gg/8KdNwwFFbA</a> or to a staff'
+        }
+    },
+    {
+        rule:{
+            text:'These rules can change at anytime, it is your job to know the rules, a staff memeber will remind you of the rules if you break them'
+        }
     }
 ]
-
+var number = 1000;
 function load() {
     for (var rule in rules) {
         var ruleText = [];
@@ -19,11 +49,12 @@ function load() {
         var obj = rules[rule];
         for (var text in obj) {
             console.log(obj);
-            ruleText += `
-            <li class="list-group-item" data-aos="fade-up" data-aos-delay="1000">${obj[text].text}</li>
+            ruleText += `<br>
+            <li class="list-group-item" data-aos="fade-up" data-aos-delay="${number}">${obj[text].text}</li>
             `;
         }
         document.getElementById("ruleList").innerHTML += `${ruleText}`;
+        number += 100;
     }
 }
 load()
